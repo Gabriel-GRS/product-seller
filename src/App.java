a2fb2d51e2a618a05b9d2f2c7bd6d98947ff9c1a
Gabriel Soares <gabrielrs.job@gmail.com>
2023-07-29 13:48:04 -0300

    feat: models iniciais feitos

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import models.Customer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        Customer customer1 = new Customer("Gabriel", 290.2);
+    
+    }
+}
---
+++ src/models/Stock.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Stock {
+
+    ArrayList<Product> products;
+
+    public Stock(ArrayList<Product> products) {
+        this.products = products;
+    }
+    
+}
